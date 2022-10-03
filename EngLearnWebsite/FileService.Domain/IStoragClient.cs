5b16df2a14eb5a7e2feec9d2ea3ae1f5dce5e286
Monodytheone <monody_tfr@qq.com>
2022-10-03 21:26:54 +0800

    FileService.Domain  developing 1

---
+++ EngLearnWebsite/FileService.Domain/IStoragClient.cs
@@ -0,0 +1,18 @@
+﻿
+namespace FileService.Domain;
+
+/// <summary>
+/// 存储服务器接口
+/// </summary>
+public interface IStoragClient
+{
+    StorageType StorageType { get; }
+
+    /// <summary>
+    /// 保存文件
+    /// </summary>
+    /// <param name="key">文件的key（一般是文件路径的一部分）</param>
+    /// <param name="content">文件内容</param>
+    /// <returns>保存的文件的全路径（可以被访问的文件Url）</returns>
+    Task<Uri> SaveAsync(string key, Stream content, CancellationToken cancellationToken = default);
+}
