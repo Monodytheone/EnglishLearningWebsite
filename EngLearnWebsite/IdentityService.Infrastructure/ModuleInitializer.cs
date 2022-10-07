72384bf46968eab2e770c52d5563082e95fde347
Monodytheone <monody_tfr@qq.com>
2022-10-07 20:44:50 +0800

    IdentityService

---
+++ EngLearnWebsite/IdentityService.Infrastructure/ModuleInitializer.cs
@@ -0,0 +1,14 @@
+﻿using IdentityService.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Zack.Commons;
+
+namespace IdentityService.Infrastructure;
+
+internal class ModuleInitializer : IModuleInitializer
+{
+    public void Initialize(IServiceCollection services)
+    {
+     
+    }
+}
---
+++ EngLearnWebsite/IdentityService.Infrastructure/Services/MockSmsSender.cs
@@ -0,0 +1,21 @@
+﻿using IdentityService.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityService.Infrastructure.Services
+{
+    public class MockSmsSender : ISmsSender
+    {
+        private readonly ILogger<MockSmsSender> _logger;
+
+        public MockSmsSender(ILogger<MockSmsSender> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task SendAsync(string phoneNumber, params string[] args)
+        {
+            _logger.LogInformation("给{0}发短信, args: {1}", phoneNumber, string.Join(",", args));
+            return Task.CompletedTask;
+        }
+    }
+}
