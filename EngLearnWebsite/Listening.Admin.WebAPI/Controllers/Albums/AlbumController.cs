307c77ae913c9103ab67d7ddb09a03c8fa56695a
Monodytheone <monody_tfr@qq.com>
2022-10-22 22:25:05 +0800

    ListeningService.Admin.WebAPI--开发3

---
+++ EngLearnWebsite/Listening.Admin.WebAPI/Controllers/Albums/AlbumController.cs
@@ -0,0 +1,81 @@
+﻿using FluentValidation;
+using Listening.Admin.WebAPI.Controllers.Albums.Requests;
+using Listening.Domain;
+using Listening.Domain.Entities;
+using Listening.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Zack.ASPNETCore;
+
+namespace Listening.Admin.WebAPI.Controllers.Albums
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    [UnitOfWork(typeof(ListeningDbContext))]
+    public class AlbumController : ControllerBase
+    {
+        private readonly ListeningDomainService _domainService;
+        private readonly IListeningRepository _repository;
+        private readonly ListeningDbContext _dbContext;
+
+        // FluentValidator手动模式的校验器
+        private readonly IValidator<AlbumAddRequest> _addValidator;
+        private readonly IValidator<AlbumUpdateRequest> _updateValidator;
+
+        public AlbumController(ListeningDomainService domainService, IListeningRepository repository, ListeningDbContext dbContext, IValidator<AlbumAddRequest> addValidator, IValidator<AlbumUpdateRequest> updateValidator)
+        {
+            _domainService = domainService;
+            _repository = repository;
+            _dbContext = dbContext;
+            _addValidator = addValidator;
+            _updateValidator = updateValidator;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Album>> FindById([RequiredGuid] Guid id)
+        {
+            Album? album = await _repository.GetAlbumByIdAsync(id);
+            if (album == null)
+            {
+                return NotFound("Id不存在");
+            }
+            return Ok(album);
+        }
+
+        [HttpGet]
+        [Route("{categoryId}")]
+        public async Task<ActionResult<Album[]>> FindByCategoryId([RequiredGuid] Guid categoryId)
+        {
+            if (await _repository.GetCategoryByIdAsync(categoryId) == null)
+            {
+                return NotFound("CategoryId不存在");
+            }
+            return await _repository.GetAlbumsByCategoryIdAsync(categoryId);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Guid>> Add(AlbumAddRequest request)
+        {
+            var validationResult = await _addValidator.ValidateAsync(request);
+            if (validationResult.IsValid == false)
+            {
+                return BadRequest(validationResult.Errors.Select(error => error.ErrorMessage));
+            }
+
+            Album album = await _domainService.AddAlbumAsync(request.CategoryId, request.Name);
+            _dbContext.Albums.Add(album);
+            return album.Id;
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public Task<ActionResult> Update([RequiredGuid] Guid id, AlbumUpdateRequest request)
+        {
+
+        }
+    }
+}
---
+++ EngLearnWebsite/Listening.Admin.WebAPI/Controllers/Albums/Requests/AlbumAddRequest.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using Listening.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Zack.DomainCommons.Models;
+
+namespace Listening.Admin.WebAPI.Controllers.Albums.Requests
+{
+    public record AlbumAddRequest(Guid CategoryId, MultilingualString Name);
+
+    public class AlbumAddRequestValidator : AbstractValidator<AlbumAddRequest>
+    {
+        public AlbumAddRequestValidator(ListeningDbContext dbContext)
+        {
+            RuleFor(x => x.CategoryId).NotEmpty().NotEqual(Guid.Empty)
+                .MustAsync((cId, ct) => dbContext.Categories.AnyAsync(category => category.Id == cId)).WithMessage(c => $"CategoryId = {c.CategoryId}不存在");  // 异步的校验规则，只支持手动校验模式
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name.Chinese).NotEmpty().Length(1, 200);
+            RuleFor(x => x.Name.English).NotNull().NotEmpty().Length(1, 200);
+        }
+    }
+}
